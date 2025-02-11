c5034de61324935ff6aba545be038d78c483f4fd
heesangs <heesangs84@gmail.com>
2025-02-11 21:58:24 +0900

    app styling

--- App.tsx
+++ App.tsx
@@ -1,47 +1,41 @@
+import { StyleSheet, Text, View, Button } from "react-native";
 
-import React,{useState} from 'react';
-import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
-
-const ViewBoxesWithColorAndText = () => {
-  const [titleText, setTitleText] = useState("heesangs's zzang");
-  const bodyText = 'I am so happy.';
-
-  const onPressTitle = () => {
-    setTitleText("Bird's Nest[pressed]");
-  };
-
-  return(
-    <SafeAreaView style={{flex: 1}}>
-      <Text style={styles.baseText}>
-        <Text style={styles.titleText} onPress={onPressTitle}>
-          {titleText}
-          {'\n'}
-          {'\n'}
-        </Text>
-        <Text numberOfLines={5}>{bodyText}</Text>
-      </Text>
-      <View
-        style={{
-          flexDirection:'row',
-          height: 100,
-          padding: 20,
-        }}>
-          <View style={{backgroundColor:'blue', flex:1}}/>
-          <View style={{backgroundColor: 'red', flex:1}}/>
-          <Text>Hello world!</Text>
-        </View>
-      </SafeAreaView>
-  );
-};
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.dummyText}>
+        <Text>Hello, World!</Text>
+      </View>
+      <Text style={styles.dummyText}>I am programer</Text>
+      <View style={styles.tableWrap}>
+        <Button title="click here!"/>
+        <Button title="click here!"/>
+        <Button title="click here!"/>
+        <Button title="click here!"/>
+        <Button title="click here!"/>
+      </View>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-  baseText: {
-    fontFamily: 'Cochin',
+  container : {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  titleText: {
-    fontSize: 20,
-    fontWeight: 'bold'
+  dummyText: {
+    margin: 16,
+    borderColor: '#4994EC',
+    borderWidth: 2,
+    padding: 16,
+  },
+  tableWrap: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    gap: 4,
   }
-});
-
-export default ViewBoxesWithColorAndText;
+})
